14cbd954d34a6c132b8064ad2ee3bbc58a69f74e
@trast2801 <trast2801@yandex.ru>
2024-11-12 13:19:25 +0300

    Добавлена возможность пользователю менять набор символов для перекодировки

---
+++ bot.py
@@ -0,0 +1,142 @@
+import telebot
+from PIL import Image
+import io
+from telebot import types
+import config
+
+TOKEN = config.TOKEN
+
+#TOKEN = '<token goes here>'
+
+bot = telebot.TeleBot(TOKEN)
+
+user_states = {}  # тут будем хранить информацию о действиях пользователя
+
+# набор символов из которых составляем изображение
+ASCII_CHARS = '@%#*+=-:. '
+
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    return image.resize((new_width, new_height))
+
+
+def grayify(image):
+    return image.convert("L")
+
+
+def image_to_ascii(image_stream, new_width=40):
+    # Переводим в оттенки серого
+    image = Image.open(image_stream).convert('L')
+
+    # меняем размер сохраняя отношение сторон
+    width, height = image.size
+    aspect_ratio = height / float(width)
+    new_height = int(
+        aspect_ratio * new_width * 0.55)  # 0,55 так как буквы выше чем шире
+    img_resized = image.resize((new_width, new_height))
+
+    img_str = pixels_to_ascii(img_resized)
+    img_width = img_resized.width
+
+    max_characters = 4000 - (new_width + 1)
+    max_rows = max_characters // (new_width + 1)
+
+    ascii_art = ""
+    for i in range(0, min(max_rows * img_width, len(img_str)), img_width):
+        ascii_art += img_str[i:i + img_width] + "\n"
+
+    return ascii_art
+
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = ""
+    for pixel in pixels:
+        characters += ASCII_CHARS[pixel * len(ASCII_CHARS) // 256]
+    return characters
+
+
+# Огрубляем изображение
+def pixelate_image(image, pixel_size):
+    image = image.resize(
+        (image.size[0] // pixel_size, image.size[1] // pixel_size),
+        Image.NEAREST
+    )
+    image = image.resize(
+        (image.size[0] * pixel_size, image.size[1] * pixel_size),
+        Image.NEAREST
+    )
+    return image
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Пришлите мне изображение, и я предложу вам варианты")
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    bot.reply_to(message, "У меня есть ваша фотография! Пожалуйста, выберите, что бы вы хотели с ней сделать.",
+                 reply_markup=get_options_keyboard())
+    user_states[message.chat.id] = {'photo': message.photo[-1].file_id}
+
+
+def get_options_keyboard():
+    '''вывод меню для выбора режима обработки изображения пользователем'''
+    keyboard = types.InlineKeyboardMarkup()
+    pixelate_btn = types.InlineKeyboardButton("Pixelate", callback_data="pixelate")
+    ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
+    change_ASCII = types.InlineKeyboardButton("Сменит набор ASCII", callback_data="change_ascii")
+    keyboard.add(pixelate_btn, ascii_btn, change_ASCII)
+    return keyboard
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    ''' отбработка выбора пользователя '''
+    if call.data == "pixelate":
+        bot.answer_callback_query(call.id, "Пикселизация вашего изображения...")
+        pixelate_and_send(call.message)
+    elif call.data == "ascii":
+        bot.answer_callback_query(call.id, "Преобразование вашего изображения в формат ASCII...")
+        ascii_and_send(call.message)
+    elif call.data == "change_ascii":
+        msg = bot.send_message(call.message.chat.id, 'Введите набор символов')
+        bot.register_next_step_handler(msg, ch_asc)
+        get_options_keyboard()
+def ch_asc(message):
+    ''' присавивает новое значение набору символов '''
+
+    global ASCII_CHARS
+    ASCII_CHARS = message.text
+
+
+def pixelate_and_send(message):
+    photo_id = user_states[message.chat.id]['photo']
+    file_info = bot.get_file(photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_stream = io.BytesIO(downloaded_file)
+    image = Image.open(image_stream)
+    pixelated = pixelate_image(image, 20)
+
+    output_stream = io.BytesIO()
+    pixelated.save(output_stream, format="JPEG")
+    output_stream.seek(0)
+    bot.send_photo(message.chat.id, output_stream)
+
+
+def ascii_and_send(message):
+    photo_id = user_states[message.chat.id]['photo']
+    file_info = bot.get_file(photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_stream = io.BytesIO(downloaded_file)
+    ascii_art = image_to_ascii(image_stream)
+    bot.send_message(message.chat.id, f"```\n{ascii_art}\n```", parse_mode="MarkdownV2")
+
+
+bot.polling(none_stop=True)
