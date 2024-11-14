4e9cbdf22e6277ee9122b6b12543e15e707489e0
@trast2801 <trast2801@yandex.ru>
2024-11-14 20:40:29 +0300

    Добавил тепловую карту вынес повторяющийся код в отдельную функцию  maket_for_processing_image(message, type_func) где type_func - значение для функций по обработке изобрадения

--- bot.py
+++ bot.py
@@ -23,31 +23,24 @@ def resize_image(image, new_width=100):
     new_height = int(new_width * ratio)
     return image.resize((new_width, new_height))
 
-
 def grayify(image):
     '''Преобразует цветное изображение в оттенки серого.'''
     return image.convert("L")
 
+
 def invert_colors(image):
-    '''Используется функция invert_colors, которая применяет
+    '''
+    Используется функция invert_colors, которая применяет
     ImageOps.invert из PIL (Python Imaging Library) к изображению.
     '''
     return ImageOps.invert(image)
 
-def send_photo(message):
-    '''Преобразует изображение в негатив и отправляет результат'''
-    photo_id = user_states[message.chat.id]['photo']
-    file_info = bot.get_file(photo_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-
-    image_stream = io.BytesIO(downloaded_file)
-    image = Image.open(image_stream)
-    inverted = invert_colors(image)
-
-    output_stream = io.BytesIO()
-    inverted.save(output_stream, format="JPEG")
-    output_stream.seek(0)
-    bot.send_photo(message.chat.id, output_stream)
+def convert_to_heatmap(image):
+    '''
+    Изображение преобразуется так, чтобы его цвета отображались в виде тепловой карты,
+     от синего (холодные области) до красного (теплые области)
+    '''
+    return ImageOps.colorize(image.convert('L'), black='blue', white='red', mid='#984f4f', midpoint=127)
 
 def image_to_ascii(image_stream, new_width=40):
     '''
@@ -107,7 +100,6 @@ def pixelate_image(image, pixel_size):
 def send_welcome(message):
     bot.reply_to(message, "Пришлите мне изображение, и я предложу вам варианты")
 
-
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     bot.reply_to(message, "У меня есть ваша фотография! Пожалуйста, выберите, что бы вы хотели с ней сделать.",
@@ -122,10 +114,11 @@ def get_options_keyboard():
     ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
     change_ASCII = types.InlineKeyboardButton("Сменить набор ASCII", callback_data="change_ascii")
     image_negative = types.InlineKeyboardButton("Получить Негатив", callback_data="invert_colors")
-    image_FLIP_LEFT_RIGHT = types.InlineKeyboardButton("Зеркало горизонт", callback_data="flip_left_right")
-    image_FLIP_TOP_BOTTOM = types.InlineKeyboardButton("Зеркало по вертикаль", callback_data="flip_top_bottom")
+    image_FLIP_LEFT_RIGHT = types.InlineKeyboardButton("Зеркало горизонтали", callback_data="flip_left_right")
+    image_FLIP_TOP_BOTTOM = types.InlineKeyboardButton("Зеркало по вертикали", callback_data="flip_top_bottom")
+    heat_map_image = types.InlineKeyboardButton("Тепловая карта", callback_data="heat_map")
     keyboard.add(pixelate_btn, ascii_btn, change_ASCII, image_negative,
-                 image_FLIP_LEFT_RIGHT,image_FLIP_TOP_BOTTOM, row_width=2)
+                 image_FLIP_LEFT_RIGHT, image_FLIP_TOP_BOTTOM,heat_map_image, row_width=2)
     return keyboard
 
 
@@ -136,7 +129,7 @@ def callback_query(call):
     '''
     if call.data == "pixelate":
         bot.answer_callback_query(call.id, "Пикселизация вашего изображения...")
-        pixelate_and_send(call.message)
+        maket_for_processing_image(call.message, "pixelate")
     elif call.data == "ascii":
         bot.answer_callback_query(call.id, "Преобразование вашего изображения в формат ASCII...")
         ascii_and_send(call.message)
@@ -146,39 +139,23 @@ def callback_query(call):
         get_options_keyboard()
     elif call.data == "invert_colors":
         bot.answer_callback_query(call.id, "Преобразование вашего изображения в негатив...")
-        send_photo(call.message)
+        maket_for_processing_image(call.message, "inverted")
     elif call.data == "flip_left_right":
         bot.answer_callback_query(call.id, "Отражение вашего изображения по горизонтали...")
-        mirror_image(call.message,rotate="FLIP_TOP_BOTTOM")
+        maket_for_processing_image(call.message,"FLIP_TOP_BOTTOM")
     elif call.data == "flip_top_bottom":
         bot.answer_callback_query(call.id, "Отражение вашего изображения по горизонтали...")
-        mirror_image(call.message,rotate="FLIP_LEFT_RIGHT")
+        maket_for_processing_image(call.message, "FLIP_LEFT_RIGHT")
+    elif call.data == "heat_map":
+        bot.answer_callback_query(call.id, "Тепловая карта вашего изображения...")
+        maket_for_processing_image(call.message, "heat_map")
+
 
 def ch_asc(message):
     ''' присваивает новое значение набору символов, учавствуют только уникальные символы '''
 
     global ASCII_CHARS
-    ASCII_CHARS = message.text
-    print(f'перед {ASCII_CHARS}')
-    ASCII_CHARS = list(set(ASCII_CHARS))
-    print(f'после {ASCII_CHARS}')
-
-
-def pixelate_and_send(message):
-    '''Пикселизирует изображение и отправляет его обратно пользователю'''
-    photo_id = user_states[message.chat.id]['photo']
-    file_info = bot.get_file(photo_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-
-    image_stream = io.BytesIO(downloaded_file)
-    image = Image.open(image_stream)
-    pixelated = pixelate_image(image, 20)
-
-    output_stream = io.BytesIO()
-    pixelated.save(output_stream, format="JPEG")
-    output_stream.seek(0)
-    bot.send_photo(message.chat.id, output_stream)
-
+    ASCII_CHARS = list(set(message.text))
 
 def ascii_and_send(message):
     '''Преобразует изображение в ASCII-арт и отправляет результат в виде текстового сообщения.'''
@@ -190,8 +167,7 @@ def ascii_and_send(message):
     ascii_art = image_to_ascii(image_stream)
     bot.send_message(message.chat.id, f"```\n{ascii_art}\n```", parse_mode="MarkdownV2")
 
-def mirror_image(message, rotate):
-    '''Создает отраженную копию изображения по горизонтали или вертикали.'''
+def maket_for_processing_image(message, type_func):
     photo_id = user_states[message.chat.id]['photo']
     file_info = bot.get_file(photo_id)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -199,13 +175,19 @@ def mirror_image(message, rotate):
     image_stream = io.BytesIO(downloaded_file)
     image = Image.open(image_stream)
 
-    if rotate == "FLIP_TOP_BOTTOM":
-        image_inverted = image.transpose(method=Image.Transpose.FLIP_TOP_BOTTOM)
-    elif rotate == "FLIP_LEFT_RIGHT":
-        image_inverted = image.transpose(method=Image.Transpose.FLIP_LEFT_RIGHT)
+    if type_func == "heat_map":
+        rez_image = convert_to_heatmap(image)
+    if type_func == "FLIP_TOP_BOTTOM":
+        rez_image = image.transpose(method=Image.Transpose.FLIP_TOP_BOTTOM)
+    if type_func == "FLIP_LEFT_RIGHT":
+        rez_image = image.transpose(method=Image.Transpose.FLIP_LEFT_RIGHT)
+    if type_func == "pixelate":
+        rez_image = pixelate_image(image, 20)
+    if type_func == "inverted":
+        rez_image = invert_colors(image)
 
     output_stream = io.BytesIO()
-    image_inverted.save(output_stream, format="JPEG")
+    rez_image.save(output_stream, format="JPEG")
     output_stream.seek(0)
     bot.send_photo(message.chat.id, output_stream)
 
